a1f71d5b00682b2b811ebda276ff992268df246a
aeksei <aeksei94@gmail.com>
2024-10-14 17:38:46 +0300

    подготовка перед лекцией

--- books/models.py
+++ books/models.py
@@ -1,20 +1,50 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
-    class BookTypeChoices(models.TextChoices):
+    class TypeChoices(models.TextChoices):
         HARDCOVER = "HC", "Твёрдый переплёт"
         PAPERCOVER = "PB", "Мягкий переплёт"
         EBOOK = "EB", "Электроная книга"
 
     title = models.CharField(max_length=100)
-    year = models.IntegerField()
+    year = models.PositiveSmallIntegerField()
     description = models.TextField(default="")
     book_type = models.CharField(
         max_length=2,
-        choices=BookTypeChoices.choices,
-        default=BookTypeChoices.PAPERCOVER,
+        choices=TypeChoices.choices,
+        default=TypeChoices.PAPERCOVER,
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=100, unique=True)
+    rating = models.FloatField(default=0)
+
+    class Meta:
+        verbose_name = "книга"
+        verbose_name_plural = "книги"
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "тег"
+        verbose_name_plural = "теги"
+
+    def __str__(self):
+        return self.name
--- books_library/settings.py
+++ books_library/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "books",
 ]
 
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "books_library.urls"
@@ -122,3 +124,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    # список IP-адресов
+    '127.0.0.1',
+    'localhost',
+]
